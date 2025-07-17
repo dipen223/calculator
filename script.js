9d80a7399f630dd92bf16ce7fb1b65329f69083d
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-07-17 17:37:02 -0500

    Add functions to add,subtract,multiply,and divide

---
+++ script.js
@@ -0,0 +1,29 @@
+const add = (a,b) => {
+    return a + b;
+}
+
+const subtract = (a,b) => {
+    return a-b;
+
+}
+
+const multiply = (a,b) => {
+    return a * b;
+
+
+}
+
+const divide = (a,b) => {
+    return a / b;
+
+}
+
+let operand1;
+let operator1;
+let operand2;
+
+const operator  = (val1,val2,operator) => {
+
+    add(operand1,operand2);
+
+}
