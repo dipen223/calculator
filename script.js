5359925460d96c8e4419be086396421618ba98a7
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-07-18 17:50:14 -0500

    Add a logic to operate on two operands and update an operand if any other value is clicked

--- script.js
+++ script.js
@@ -1,88 +1,100 @@
-const buttons  = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
 const clearBtn = document.querySelector(".clear");
 const displayResult = document.getElementById("result");
 
-const add = (a,b) => {
+const add = (a, b) => {
     return a + b;
 }
 
-const subtract = (a,b) => {
-    return a-b;
+const subtract = (a, b) => {
+    return a - b;
 
 }
 
-const multiply = (a,b) => {
+const multiply = (a, b) => {
     return a * b;
 
 
 }
 
-const divide = (a,b) => {
+const divide = (a, b) => {
     return a / b;
 
 }
 
-let operand1;
-let operator;
-let operand2;
+let operand1 = null;
+let operator = null;
+let operand2 = null;
 let currentInput = "";
 
-const operate  = (val1,val2,operator) => {
-//  write a code to check which operator has been passed
-    if(operator === "+")
-    {
-        return add(val1,val2);
+const operate = (val1, val2, operator) => {
+    //  write a code to check which operator has been passed
+    if (operator === "+") {
+        return add(val1, val2);
 
     }
-    else if(operator === "-"){
-        return subtract(val1,val2);
+    else if (operator === "-") {
+        return subtract(val1, val2);
     }
-    else if(operator === "X"){
-        return multiply(val1,val2);
+    else if (operator === "X") {
+        return multiply(val1, val2);
     }
-    else if(operator === "/"){
-        return divide(val1,val2);
+    else if (operator === "/") {
+        return divide(val1, val2);
     }
-    
+
 
 }
 
 
-const displayPopulator = () =>{
+const displayPopulator = () => {
 
-const arrBtns = [...buttons];
+    const arrBtns = [...buttons];
 
 
 
-arrBtns.forEach((btn) => {
-    btn.addEventListener("click", ()=> {
-        const btnVal = btn.innerText;
+    arrBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const btnVal = btn.innerText;
 
-        if(!isNaN(btnVal)){
-        currentInput += btnVal;
-        displayResult.innerText += btnVal;
-    }
-    else if(["+","-","/","X"].includes(btnVal)){
-        operand1 = Number(currentInput);
-        operator = btnVal;
-        currentInput="";
-        displayResult.innerText += `${btnVal}`
-    }
-    else if(btnVal === "="){
-        operand2 = Number(currentInput);
-        const result = operate(operand1,operand2,operator);
-        displayResult.innerText  = result;
-        currentInput = result.toString();
-        operand1 = null;
-        operand2 = null;
-    }
-    
+            if (!isNaN(btnVal)) {
+                currentInput += btnVal;
+                displayResult.innerText += btnVal;
+            }
+            else if (["+", "-", "/", "X"].includes(btnVal)) {
+                if (operand1 !== null && operator !== null && currentInput !== "") {
+                    operand2 = Number(currentInput);
+                    const result = operate(operand1, operand2, operator);
+                    operand1 = result;
+                    displayResult.innerText = result + btnVal;
+
+                }
+                else if(currentInput !== ""){
+                    operand1 = Number(currentInput);
+                    displayResult.innerText += btnVal;
+                }
+                else{
+                    return;
+                }
+                operator = btnVal;
+                currentInput = "";
+
+            }
+            else if (btnVal === "=") {
+                operand2 = Number(currentInput);
+                const result = operate(operand1, operand2, operator);
+                displayResult.innerText = result;
+                currentInput = result.toString();
+                operand1 = null;
+                operand2 = null;
+            }
+
+
+        });
+
+
 
     });
-
- 
-    
-});
 
 };
 
@@ -95,8 +107,8 @@ const clearDisplay = () => {
     operand1 = null;
     operand2 = null;
     operator = "";
-    currentInput="";
+    currentInput = "";
 
 }
 
-clearBtn.addEventListener("click",clearDisplay);
+clearBtn.addEventListener("click", clearDisplay);
