c6c4cd2277fc3c78b6e2e031d038c097220a6bc6
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-07-18 18:06:54 -0500

    divison by zero doesn't allow the user to press any buttons nuntil user clicks clear btn

--- script.js
+++ script.js
@@ -2,117 +2,85 @@ const buttons = document.querySelectorAll("button");
 const clearBtn = document.querySelector(".clear");
 const displayResult = document.getElementById("result");
 
-const add = (a, b) => {
-    return a + b;
-}
-
-const subtract = (a, b) => {
-    return a - b;
-
-}
-
-const multiply = (a, b) => {
-    return a * b;
-
-
-}
-
-const divide = (a, b) => {
-    return a / b;
-
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 let operand1 = null;
 let operator = null;
 let operand2 = null;
 let currentInput = "";
+let errorOccurred = false;
 
 const operate = (val1, val2, operator) => {
-    //  write a code to check which operator has been passed
-    if (operator === "+") {
-        return add(val1, val2);
-
-    }
-    else if (operator === "-") {
-        return subtract(val1, val2);
-    }
-    else if (operator === "X") {
-        return multiply(val1, val2);
-    }
+    if (operator === "+") return add(val1, val2);
+    else if (operator === "-") return subtract(val1, val2);
+    else if (operator === "X") return multiply(val1, val2);
     else if (operator === "/") {
+        if (val2 === 0) {
+            errorOccurred = true;
+            return "Nice try, genius. Division by zero is still illegal in math.";
+        }
         return divide(val1, val2);
     }
-
-
-}
-
+};
 
 const displayPopulator = () => {
-
-    const arrBtns = [...buttons];
-
-
-
+    const arrBtns = [...buttons];  
     arrBtns.forEach((btn) => {
         btn.addEventListener("click", () => {
             const btnVal = btn.innerText;
+
+            
+            if (errorOccurred && btnVal !== "Clear") return;
 
             if (!isNaN(btnVal)) {
                 currentInput += btnVal;
                 displayResult.innerText += btnVal;
             }
+
+            
             else if (["+", "-", "/", "X"].includes(btnVal)) {
                 if (operand1 !== null && operator !== null && currentInput !== "") {
                     operand2 = Number(currentInput);
                     const result = operate(operand1, operand2, operator);
                     operand1 = result;
                     displayResult.innerText = result + btnVal;
-
-                }
-                else if(currentInput !== ""){
+                } else if (currentInput !== "") {
                     operand1 = Number(currentInput);
                     displayResult.innerText += btnVal;
-                }
-                else{
-                    return;
+                } else {
+                    return; 
                 }
                 operator = btnVal;
                 currentInput = "";
-
             }
+
+           
             else if (btnVal === "=") {
-                if(operand1 !== null && operator !== null && currentInput !== ""){
-
-                
-                operand2 = Number(currentInput);
-                const result = operate(operand1, operand2, operator);
-                displayResult.innerText = result;
-                currentInput = result.toString();
-                operand1 = null;
-                operand2 = null;
+                if (operand1 !== null && operator !== null && currentInput !== "") {
+                    operand2 = Number(currentInput);
+                    const result = operate(operand1, operand2, operator);
+                    displayResult.innerText = result;
+                    currentInput = result.toString();
+                    operand1 = null;
+                    operand2 = null;
+                }
             }
-        }
-
-
         });
-
-
-
     });
-
 };
 
 displayPopulator();
-
-
 
 const clearDisplay = () => {
     displayResult.innerText = "";
     operand1 = null;
     operand2 = null;
-    operator = "";
+    operator = null;
     currentInput = "";
-
-}
+    errorOccurred = false;
+};
 
 clearBtn.addEventListener("click", clearDisplay);
